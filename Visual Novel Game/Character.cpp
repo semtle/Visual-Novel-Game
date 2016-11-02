3935ccb94fb6caf3cb77a7f43558ce17f333f755
Zentryn <veikkis98@gmail.com>
2016-11-03 00:31:41 +0200

    Added additional images for Teemu-kun and Makoto-kun

--- Visual Novel Game/Character.cpp
+++ Visual Novel Game/Character.cpp
@@ -15,6 +15,30 @@ Character::Character(const std::string& name, float Influence)
 		std::cout << "Read file successfully.\n";
 	}
 	else std::cout << "Unable to open character image path file!\n";
+
+	// Makoto-kun has some extra images :-)
+	if (name == "Makoto-kun") {
+		this->images.emplace("bored_uniform", Bengine::ResourceManager::getTexture("Textures/Characters/Makoto-kun/bored_uniform.png").id);
+		this->images.emplace("bored_freetime", Bengine::ResourceManager::getTexture("Textures/Characters/Makoto-kun/bored_freetime.png").id);
+
+		this->images.emplace("smiling_side_uniform", Bengine::ResourceManager::getTexture("Textures/Characters/Makoto-kun/smiling_side_uniform.png").id);
+		this->images.emplace("smiling_side_freetime", Bengine::ResourceManager::getTexture("Textures/Characters/Makoto-kun/smiling_side_freetime.png").id);
+
+		this->images.emplace("confident_uniform", Bengine::ResourceManager::getTexture("Textures/Characters/Makoto-kun/confident_uniform.png").id);
+		this->images.emplace("confident_freetime", Bengine::ResourceManager::getTexture("Textures/Characters/Makoto-kun/confident_freetime.png").id);
+
+		this->images.emplace("shouting_uniform", Bengine::ResourceManager::getTexture("Textures/Characters/Makoto-kun/shouting_uniform.png").id);
+		this->images.emplace("shouting_freetime", Bengine::ResourceManager::getTexture("Textures/Characters/Makoto-kun/shouting_freetime.png").id);
+
+		this->images.emplace("disappointed_uniform", Bengine::ResourceManager::getTexture("Textures/Characters/Makoto-kun/disappointed_uniform.png").id);
+		this->images.emplace("disappointed_freetime", Bengine::ResourceManager::getTexture("Textures/Characters/Makoto-kun/disappointed_freetime.png").id);
+	}
+
+	// Teemu-kun also has something extra ;-)
+	if (name == "Teemu-kun") {
+		this->images.emplace("happy_uniform", Bengine::ResourceManager::getTexture("Textures/Characters/Teemu-kun/happy_uniform.png").id);
+		this->images.emplace("happy_freetime", Bengine::ResourceManager::getTexture("Textures/Characters/Teemu-kun/happy_freetime.png").id);
+	}
 }
 
 

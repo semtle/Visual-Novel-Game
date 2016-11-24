5e6d6d97dd683b636ce0fac10b1fef2232fc0944
Zentryn <veikkis98@gmail.com>
2016-11-24 23:03:26 +0200

    Changed window size, added new function for drawing

--- StoryEditor/MainProgram.h
+++ StoryEditor/MainProgram.h
@@ -14,7 +14,7 @@
 #include <Bengine/Camera2D.h>
 #include <Bengine/SpriteFont.h>
 
-enum class ProgramState { RUN, EXIT };
+enum class ProgramState { MAINSCREEN, ADDSCENE, EXIT };
 
 class MainProgram
 {
@@ -28,23 +28,41 @@ public:
 	void gameLoop();
 	void processInput();
 	void drawScreen();
+
+	void checkMainScreenInputs();
+	void checkSceneCreationScreenInputs();
+	void onKeyPress(unsigned int keyID);
+
+	void drawMainScreen();
+	void drawSceneCreationScreen();
+
+	void drawSceneCreationScreenTexts();
 private:
 	Bengine::Window window; ///< Main window
 
 	Bengine::GLSLProgram shaderProgram; ///< Shaders
 
 	Bengine::SpriteBatch spriteBatch; ///< Main sprite batch
+	Bengine::SpriteBatch fontBatch; ///< Batch for fonts
 
 	Bengine::InputManager inputManager; ///< Main input manager
 
 	Bengine::Camera2D camera; ///< Main camera
+	Bengine::Camera2D fontCamera; ///< Camera for texts
 
 	Bengine::SpriteFont* spriteFont; ///< Main font
 
 	SceneManager* sceneManager; ///< Scene manager
 
-	int screenWidth = 1024;
-	int screenHeight = 768;
+	int screenWidth = 800;
+	int screenHeight = 600;
 
-	ProgramState currentState = ProgramState::RUN;
+	std::string currentSceneName = "";
+
+	static const int BUTTONS_MIDDLE_HORIZONTAL_RADIUS = 60;
+	static const int BUTTONS_MIDDLE_VERTICAL_RADIUS = -150;
+	static const int BUTTON_WIDTH = 149;
+	static const int BUTTON_HEIGHT = 38;
+
+	ProgramState currentState = ProgramState::MAINSCREEN;
 };

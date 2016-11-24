25378718ece376c622a6f9a855a45a406a1312a3
Zentryn <veikkis98@gmail.com>
2016-11-24 23:03:41 +0200

    Changed window size, added new function for drawing and input processing

--- StoryEditor/MainProgram.cpp
+++ StoryEditor/MainProgram.cpp
@@ -33,14 +33,18 @@ void MainProgram::initSystems()
 	// Initialize camera
 	this->camera.init(this->screenWidth, this->screenHeight);
 
+	this->fontCamera.init(this->screenWidth, this->screenHeight);
+	this->fontCamera.setPosition(glm::vec2(this->screenWidth / 2, this->screenHeight / 2));
+
 	// Initialize the spritebatch
 	this->spriteBatch.init();
+	this->fontBatch.init();
 
 	// Initialize font
 	this->spriteFont = new Bengine::SpriteFont("Fonts/Chapaza/Chapaza.ttf", 32);
 
 	// Initialize the scene manager
-	this->sceneManager = new SceneManager(&this->inputManager);
+	this->sceneManager = new SceneManager();
 
 	// Release all keys used in the game because release mode somehow has them already pressed
 	this->inputManager.releaseKey(SDL_BUTTON_LEFT);
@@ -60,14 +64,28 @@ void MainProgram::initShaders()
 
 void MainProgram::gameLoop()
 {
-	while (this->currentState == ProgramState::RUN) {
-		this->camera.update();
-		this->inputManager.update();
-		this->sceneManager->update();
+	while (this->currentState != ProgramState::EXIT) {
+		while (this->currentState == ProgramState::MAINSCREEN) {
+			this->camera.update();
+			this->inputManager.update();
 
-		this->processInput();
+			this->processInput();
+			this->checkMainScreenInputs();
 
-		this->drawScreen();
+			this->drawScreen();
+		}
+		
+		while (this->currentState == ProgramState::ADDSCENE) {
+			this->camera.update();
+			this->fontCamera.update();
+			this->inputManager.update();
+			this->processInput();
+			this->checkSceneCreationScreenInputs();
+
+			this->drawScreen();
+		}
+
+		this->currentSceneName = "";
 	}
 }
 
@@ -81,6 +99,13 @@ void MainProgram::processInput()
 		case SDL_QUIT:
 			this->currentState = ProgramState::EXIT;
 			break; 
+		case SDL_KEYDOWN:
+			this->inputManager.pressKey(event.key.keysym.sym);
+			this->onKeyPress(event.key.keysym.sym);
+			break;
+		case SDL_KEYUP:
+			this->inputManager.releaseKey(event.key.keysym.sym);
+			break;
 		case SDL_MOUSEMOTION:
 			this->inputManager.setMouseCoords(static_cast<float>(event.motion.x), static_cast<float>(event.motion.y));
 			break;
@@ -91,6 +116,94 @@ void MainProgram::processInput()
 			this->inputManager.releaseKey(event.button.button);
 			break;
 		}
+	}
+}
+
+
+void MainProgram::checkMainScreenInputs()
+{
+	glm::vec2 mouseCoords = this->inputManager.getMouseCoords();
+
+	// Mouse is inside the 'add new' button
+	if (mouseCoords.x > 34 && mouseCoords.x < 168) {
+		if (mouseCoords.y > this->screenHeight - 68 && mouseCoords.y < this->screenHeight - 30) {
+			if (this->inputManager.isKeyDown(SDL_BUTTON_LEFT)) {
+				this->inputManager.releaseKey(SDL_BUTTON_LEFT);
+				this->currentState = ProgramState::ADDSCENE;
+			}
+		}
+	}
+}
+
+
+void MainProgram::checkSceneCreationScreenInputs()
+{
+	glm::vec2 mouseCoords = this->inputManager.getMouseCoords();
+
+	// Get cancel button's location
+	static const int cancelButtonX = this->screenWidth / 2 + this->BUTTONS_MIDDLE_HORIZONTAL_RADIUS;
+	static const int cancelButtonY = this->screenHeight / 2 + abs(this->BUTTONS_MIDDLE_VERTICAL_RADIUS) - this->BUTTON_HEIGHT;
+
+	// Mouse is inside the 'Cancel' button
+	if (mouseCoords.x > cancelButtonX && mouseCoords.x < cancelButtonX + this->BUTTON_WIDTH) {
+		if (mouseCoords.y > cancelButtonY && mouseCoords.y < cancelButtonY + this->BUTTON_HEIGHT) {
+			if (this->inputManager.isKeyDown(SDL_BUTTON_LEFT)) {
+				this->inputManager.releaseKey(SDL_BUTTON_LEFT);
+				this->currentState = ProgramState::MAINSCREEN;
+			}
+		}
+	}
+}
+
+
+void MainProgram::onKeyPress(unsigned int keyID)
+{
+	// Handle modifying the current scene name being given
+	if (this->currentState == ProgramState::ADDSCENE) {
+		std::string keyName = SDL_GetKeyName(keyID);
+		static std::string allowedKeys[] = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Space", "Backspace", "Return" };
+
+		bool keyAllowed = false;
+
+		for (std::string name : allowedKeys) {
+			if (name == keyName) keyAllowed = true;
+		}
+
+		if (!keyAllowed) return;
+
+		// Remove the last character from the player's name
+		if (keyName == "Backspace") {
+			this->currentSceneName = this->currentSceneName.substr(0, this->currentSceneName.size() - 1);
+		}
+		// Add a space to the player's name
+		else if (keyName == "Space" && this->currentSceneName.length() < 16) {
+			if (this->currentSceneName.length() > 1) this->currentSceneName += " ";
+		}
+		// Confirm the player's name
+		else if (keyName == "Return") {
+			if (this->currentSceneName.length() >= 3 && this->currentSceneName.length() <= 16) {
+				std::cout << "Name was valid.\n";
+			}
+		}
+		// Add the character to the player's name
+		else {
+			if (this->currentSceneName.length() < 16) {
+				this->currentSceneName += static_cast<char>(keyID);
+			}
+		}
+
+		// Capitalize first letters of the name
+		this->currentSceneName[0] = toupper(this->currentSceneName[0]);
+
+		for (unsigned i = 0; i < this->currentSceneName.length(); i++) {
+			if (this->currentSceneName[i] == ' ') {
+				if (i != this->currentSceneName.length() - 1) {
+					this->currentSceneName[i + 1] = toupper(this->currentSceneName[i + 1]);
+				}
+			}
+		}
+
+		std::cout << "Name: " << this->currentSceneName << "\n";
 	}
 }
 
@@ -119,14 +232,36 @@ void MainProgram::drawScreen()
 	// Send the camera matrix
 	glUniformMatrix4fv(pLocation, 1, false, &cameraMatrix[0][0]);
 
+	this->spriteBatch.begin();
+
+	if (this->currentState == ProgramState::MAINSCREEN) this->drawMainScreen();
+	else if (this->currentState == ProgramState::ADDSCENE) this->drawSceneCreationScreen();
+
+	this->spriteBatch.end();
+	this->spriteBatch.renderBatch();
+
+	if (this->currentState == ProgramState::ADDSCENE) this->drawSceneCreationScreenTexts();
+
+	// Unbind the texture
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	// Deactivate the shader program
+	this->shaderProgram.unuse();
+
+	// Swap the buffer
+	this->window.swapBuffer();
+}
+
+
+void MainProgram::drawMainScreen()
+{
 	static const unsigned int main_background = Bengine::ResourceManager::getTexture("Textures/main-bg.png").id;
 	static const unsigned int add_new_button = Bengine::ResourceManager::getTexture("Textures/addnew.png").id;
 	static const glm::vec4 bgDestRect(-this->screenWidth / 2, -this->screenHeight / 2, this->screenWidth, this->screenHeight);
-	static const glm::vec4 addNewDestRect(-this->screenWidth / 2 + 50, -this->screenHeight / 2 + 30, 149, 38);
+	static const glm::vec4 addNewDestRect(-this->screenWidth / 2 + 34, -this->screenHeight / 2 + 30, 134, 34);
 	static const glm::vec4 uvRect(0.0f, 0.0f, 1.0f, 1.0f);
 	static const Bengine::ColorRGBA8 color(255, 255, 255, 255);
 
-	this->spriteBatch.begin();
 	this->spriteBatch.draw(
 		bgDestRect,
 		uvRect,
@@ -141,15 +276,80 @@ void MainProgram::drawScreen()
 		0.0f,
 		color
 	);
-	this->spriteBatch.end();
-	this->spriteBatch.renderBatch();
+}
 
-	// Unbind the texture
-	glBindTexture(GL_TEXTURE_2D, 0);
 
-	// Deactivate the shader program
-	this->shaderProgram.unuse();
+void MainProgram::drawSceneCreationScreen()
+{
+	static const unsigned int background = Bengine::ResourceManager::getTexture("Textures/newscene.png").id;
+	static const unsigned int cancelButton = Bengine::ResourceManager::getTexture("Textures/cancel-button.png").id;
+	static const unsigned int createButton = Bengine::ResourceManager::getTexture("Textures/create-button.png").id;
+	
+	static const glm::vec4 bgDestRect(-this->screenWidth / 2, -this->screenHeight / 2, this->screenWidth, this->screenHeight);
+	static const glm::vec4 cancelBtnDestRect(this->BUTTONS_MIDDLE_HORIZONTAL_RADIUS, this->BUTTONS_MIDDLE_VERTICAL_RADIUS, this->BUTTON_WIDTH, this->BUTTON_HEIGHT);
+	static const glm::vec4 createBtnDestRect(-this->BUTTONS_MIDDLE_HORIZONTAL_RADIUS - 149, this->BUTTONS_MIDDLE_VERTICAL_RADIUS, this->BUTTON_WIDTH, this->BUTTON_HEIGHT);
 
-	// Swap the buffer
-	this->window.swapBuffer();
+	static const glm::vec4 addNewDestRect(-this->screenWidth / 2 + 50, -this->screenHeight / 2 + 30, 149, 38);
+	static const glm::vec4 uvRect(0.0f, 0.0f, 1.0f, 1.0f);
+	static const Bengine::ColorRGBA8 color(255, 255, 255, 255);
+
+	// Background (banner, bg color)
+	this->spriteBatch.draw(
+		bgDestRect,
+		uvRect,
+		background,
+		0.0f,
+		color
+	);
+
+	// Cancel button
+	this->spriteBatch.draw(
+		cancelBtnDestRect,
+		uvRect,
+		cancelButton,
+		0.0f,
+		color
+	);
+
+	// Create button
+	this->spriteBatch.draw(
+		createBtnDestRect,
+		uvRect,
+		createButton,
+		0.0f,
+		color
+	);
+}
+
+
+void MainProgram::drawSceneCreationScreenTexts()
+{
+	// Make the buffer that will hold the text
+	char buffer[256];
+
+	// Set the camera matrix
+	GLint pLocation = this->shaderProgram.getUniformLocation("P");
+	glm::mat4 cameraMatrix = this->fontCamera.getCameraMatrix();
+
+	// Send the camera matrix
+	glUniformMatrix4fv(pLocation, 1, false, &cameraMatrix[0][0]);
+
+	this->fontBatch.begin();
+
+	// Fill the buffer with the text
+	sprintf_s(buffer, "%s", this->currentSceneName.c_str());
+
+	this->spriteFont->draw(
+		this->fontBatch,
+		buffer,
+		glm::vec2(this->screenWidth / 2, this->screenHeight / 2 + 50),
+		glm::vec2(1.0f),
+		0.0f,
+		Bengine::ColorRGBA8(0, 0, 0, 255),
+		Bengine::Justification::MIDDLE
+	);
+
+	this->fontBatch.end();
+	this->fontBatch.renderBatch();
+	std::cout << "drawnig\n";
 }

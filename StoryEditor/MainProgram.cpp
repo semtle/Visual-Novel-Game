e3c2107c2735250e25e356f830cd8c524e451dc7
Zentryn <veikkis98@gmail.com>
2016-11-28 23:57:48 +0200

    Added input handling for scene boxes, flags to check if dialogue or scene is being added and more

--- StoryEditor/MainProgram.cpp
+++ StoryEditor/MainProgram.cpp
@@ -82,11 +82,12 @@ void MainProgram::gameLoop()
 
 			this->processInput();
 			this->checkMainScreenInputs();
+			this->update();
 
 			this->drawScreen();
 		}
 		
-		while (this->currentState == ProgramState::ADDSCENE) {
+		while (this->currentState == ProgramState::ADDSCENE || this->currentState == ProgramState::ADD_DIALOGUE) {
 			this->camera.update();
 			this->fontCamera.update();
 			this->inputManager.update();
@@ -98,6 +99,28 @@ void MainProgram::gameLoop()
 		}
 
 		this->currentSceneName = "";
+	}
+}
+
+
+void MainProgram::update()
+{
+	this->shownSceneBlockPositions.clear();
+
+	int counter = 0;
+
+	// Store the positions of the scene boxes to check which box was clicked on
+	// Doing this every frame so that the positions are always updated.
+	for (int idx : this->shownSceneIndexes) {
+		this->shownSceneBlockPositions.push_back(
+			std::pair<int, glm::vec2>
+			(
+				idx,
+				glm::vec2(this->sceneBlockDrawStartPos.x, this->sceneBlockDrawStartPos.y - (this->sceneBlockPositionDiff * counter))
+			)
+		);
+
+		counter++;
 	}
 }
 
@@ -115,6 +138,9 @@ void MainProgram::processInput()
 		case SDL_KEYDOWN:
 			this->inputManager.pressKey(event.key.keysym.sym);
 			this->onKeyPress(event.key.keysym.sym);
+
+			std::cout << "Current scene name: " << this->currentSceneName << "\n";
+			std::cout << "Current dialogue name: " << this->currentDialogueName << "\n";
 			break;
 		case SDL_KEYUP:
 			this->inputManager.releaseKey(event.key.keysym.sym);
@@ -173,13 +199,28 @@ void MainProgram::checkMainScreenInputs()
 	dim = this->getInputDimensions(this->downArrowDestDect);
 
 	// Mouse is inside the down arrow
-	if (this->sceneManager->getScenes().size() > 4 && this->currentSceneListIdx < this->sceneManager->getScenes().size() - 4) {
+	if (this->sceneManager->getScenes().size() > 5 && this->currentSceneListIdx < this->sceneManager->getScenes().size() - 5) {
 		if (mouseCoords.x > dim.x && mouseCoords.x < dim.x + dim.z) {
 			if (mouseCoords.y > dim.y && mouseCoords.y < dim.y + dim.a) {
 				if (this->inputManager.isKeyDown(SDL_BUTTON_LEFT)) {
 					this->inputManager.releaseKey(SDL_BUTTON_LEFT);
 
 					this->currentSceneListIdx++;
+				}
+			}
+		}
+	}
+
+	// Check if any of the scene boxes were clicked on
+	for (unsigned i = 0; i < this->shownSceneBlockPositions.size(); i++) {
+		dim = this->getInputDimensions(glm::vec4(this->shownSceneBlockPositions[i].second, this->SCENE_BOX_WIDTH, this->SCENE_BOX_HEIGHT));
+
+		if (mouseCoords.x > dim.x && mouseCoords.x < dim.x + dim.z) {
+			if (mouseCoords.y > dim.y && mouseCoords.y < dim.y + dim.a) {
+				if (this->inputManager.isKeyDown(SDL_BUTTON_LEFT)) {
+					this->inputManager.releaseKey(SDL_BUTTON_LEFT);
+
+					std::cout << "Clicked on scene box\n";
 				}
 			}
 		}
@@ -211,19 +252,39 @@ void MainProgram::checkSceneCreationScreenInputs()
 		if (mouseCoords.y > dim.y && mouseCoords.y < dim.y + dim.a) {
 			if (this->inputManager.isKeyDown(SDL_BUTTON_LEFT)) {
 				this->inputManager.releaseKey(SDL_BUTTON_LEFT);
-				
-				this->sceneManager->addScene(this->currentSceneName);
+
+				if (this->currentState == ProgramState::ADDSCENE) {
+					this->sceneManager->addScene(this->currentSceneName);
+				}
+				else if (this->currentState == ProgramState::ADD_DIALOGUE) {
+					this->sceneManager->addDialogue(this->selectedSceneIdx, &this->currentDialogue);
+				}
+
 				this->currentState = ProgramState::MAINSCREEN;
 			}
 		}
+	}
+
+	// When pressing enter, submit the new scene
+	if (this->inputManager.isKeyDown(SDLK_RETURN)) {
+		this->inputManager.releaseKey(SDL_BUTTON_LEFT);
+
+		if (this->currentState == ProgramState::ADDSCENE) {
+			this->sceneManager->addScene(this->currentSceneName);
+		}
+		else if (this->currentState == ProgramState::ADD_DIALOGUE) {
+			this->sceneManager->addDialogue(this->selectedSceneIdx, &this->currentDialogue);
+		}
+
+		this->currentState = ProgramState::MAINSCREEN;
 	}
 }
 
 
 void MainProgram::onKeyPress(unsigned int keyID)
 {
-	// Handle modifying the current scene name being given
-	if (this->currentState == ProgramState::ADDSCENE) {
+	if (this->currentState == ProgramState::ADD_DIALOGUE || this->currentState == ProgramState::ADDSCENE) {
+		// Handle modifying the current scene name being given
 		std::string keyName = SDL_GetKeyName(keyID);
 		static std::string allowedKeys[] = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Space", "Backspace", "Return" };
 
@@ -237,32 +298,77 @@ void MainProgram::onKeyPress(unsigned int keyID)
 
 		// Remove the last character from the player's name
 		if (keyName == "Backspace") {
-			this->currentSceneName = this->currentSceneName.substr(0, this->currentSceneName.size() - 1);
-		}
-		// Add a space to the player's name
-		else if (keyName == "Space" && this->currentSceneName.length() < 16) {
-			if (this->currentSceneName.length() > 1) this->currentSceneName += " ";
-		}
-		// Confirm the player's name
-		else if (keyName == "Return") {
-			if (this->currentSceneName.length() >= 3 && this->currentSceneName.length() <= 16) {
-				std::cout << "Name was valid.\n";
+			if (this->currentState == ProgramState::ADDSCENE) {
+				this->currentSceneName = this->currentSceneName.substr(0, this->currentSceneName.size() - 1);
+			}
+			else {
+				this->currentDialogueName = this->currentDialogueName.substr(0, this->currentDialogueName.size() - 1);
 			}
 		}
+
+		// Add a space to the name
+		if (this->currentState == ProgramState::ADDSCENE) {
+			if (keyName == "Space" && this->currentSceneName.length() < 16) {
+				if (this->currentSceneName.length() > 1) this->currentSceneName += " ";
+			}
+		}
+
+		else if (this->currentState == ProgramState::ADD_DIALOGUE) {
+			if (keyName == "Space" && this->currentDialogueName.length() < 16) {
+				if (this->currentDialogueName.length() > 1) this->currentDialogueName += " ";
+			}
+		}
+
+		// Confirm the player's name
+		if (keyName == "Return") {
+			if (this->currentState == ProgramState::ADDSCENE) {
+				if (this->currentSceneName.length() >= 3 && this->currentSceneName.length() <= 16) {
+					std::cout << "Name was valid.\n";
+				}
+			}
+			else {
+				if (this->currentDialogueName.length() >= 3 && this->currentDialogueName.length() <= 16) {
+					std::cout << "Name was valid.\n";
+				}
+			}
+		}
+
 		// Add the character to the player's name
-		else {
-			if (this->currentSceneName.length() < 16) {
-				this->currentSceneName += static_cast<char>(keyID);
+		if (keyName != "Return" && keyName != "Space" && keyName != "Backspace") {
+			if (this->currentState == ProgramState::ADDSCENE) {
+				if (this->currentSceneName.length() < 16) {
+					this->currentSceneName += static_cast<char>(keyID);
+				}
+			}
+			else {
+				if (this->currentDialogueName.length() < 16) {
+					this->currentDialogueName += static_cast<char>(keyID);
+				}
 			}
 		}
 
 		// Capitalize first letters of the name
-		this->currentSceneName[0] = toupper(this->currentSceneName[0]);
+		if (this->currentSceneName.length() > 0) {
+			this->currentSceneName[0] = toupper(this->currentSceneName[0]);
+		}
+		if (this->currentDialogueName.length() > 0) {
+			this->currentDialogueName[0] = toupper(this->currentDialogueName[0]);
+		}
 
+		// Scene name
 		for (unsigned i = 0; i < this->currentSceneName.length(); i++) {
 			if (this->currentSceneName[i] == ' ') {
 				if (i != this->currentSceneName.length() - 1) {
 					this->currentSceneName[i + 1] = toupper(this->currentSceneName[i + 1]);
+				}
+			}
+		}
+
+		// Dialogue name
+		for (unsigned i = 0; i < this->currentDialogueName.length(); i++) {
+			if (this->currentDialogueName[i] == ' ') {
+				if (i != this->currentDialogueName.length() - 1) {
+					this->currentDialogueName[i + 1] = toupper(this->currentDialogueName[i + 1]);
 				}
 			}
 		}
@@ -328,7 +434,7 @@ void MainProgram::drawMainScreen()
 	static const unsigned int add_new_button = Bengine::ResourceManager::getTexture("Textures/addnew.png").id;
 	static const unsigned int sceneBox = Bengine::ResourceManager::getTexture("Textures/scene-box.png").id;
 
-	std::map<int, std::pair<std::string, Dialogue>> allScenes = this->sceneManager->getScenes();
+	std::map<int, std::pair<std::string, std::vector<Dialogue *>>> allScenes = this->sceneManager->getScenes();
 
 	// BG
 	this->spriteBatch.draw(
@@ -360,7 +466,7 @@ void MainProgram::drawMainScreen()
 	}
 
 	// Down arrow
-	if (allScenes.size() > 4 && this->currentSceneListIdx < allScenes.size() - 4) {
+	if (allScenes.size() > 5 && this->currentSceneListIdx < allScenes.size() - 5) {
 		this->spriteBatch.draw(
 			this->downArrowDestDect,
 			this->mainUvRect,
@@ -369,38 +475,101 @@ void MainProgram::drawMainScreen()
 			this->color
 		);
 	}
-	
-	std::map<int, std::pair<std::string, Dialogue>> shownScenes = this->getShownScenes(allScenes);
 
-	glm::vec4 originalDestRect = sceneBoxDestRect;
-	for (unsigned i = 0; i < shownScenes.size(); i++) {
-		this->spriteBatch.draw(
-			this->sceneBoxDestRect,
-			this->mainUvRect,
-			sceneBox,
-			0.0f,
-			this->color
-		);
-		sceneBoxDestRect.y -= 87;
+	// Draw dialogue editor
+	if (this->selectedSceneIdx != -1) this->drawCurrentDialogue();
+
+	// If no scene is selected, show list of scenes
+	if (this->selectedSceneIdx == -1) {
+		std::map<int, std::pair<std::string, std::vector<Dialogue *>>> shownScenes = this->getShownScenes(allScenes);
+
+		glm::vec4 originalDestRect = sceneBoxDestRect;
+		for (unsigned i = 0; i < shownScenes.size(); i++) {
+			this->spriteBatch.draw(
+				this->sceneBoxDestRect,
+				this->mainUvRect,
+				sceneBox,
+				0.0f,
+				this->color
+			);
+			sceneBoxDestRect.y -= this->sceneBlockPositionDiff;
+		}
+		sceneBoxDestRect = originalDestRect;
 	}
-	sceneBoxDestRect = originalDestRect;
+	// If a scene is selected, show list of the dialogues
+	else {
+
+	}
+}
+
+
+void MainProgram::drawCurrentDialogue()
+{
+	static const unsigned int blackBox = Bengine::ResourceManager::getTexture("Textures/schoolyard.png").id;
+	static const unsigned int textBox = Bengine::ResourceManager::getTexture("Textures/TextBoxBlue.png").id;
+
+	/*
+	this->spriteBatch.draw(
+		this->blackBoxDestRect,
+		this->mainUvRect,
+		blackBox,
+		0.0f,
+		this->color
+	);
+
+	this->spriteBatch.draw(
+		glm::vec4(185, -290, 165, 440),
+		this->mainUvRect,
+		laugh,
+		0.0f,
+		this->color
+	);
+
+	this->spriteBatch.draw(
+		glm::vec4(-135, -290, 165, 440),
+		glm::vec4(0.0f, 0.0f, -1.0f, 1.0f),
+		laugh,
+		0.0f,
+		this->color
+	);
+
+	this->spriteBatch.draw(
+		glm::vec4(-this->screenWidth / 2 + 200, -270, 600, 500),
+		this->mainUvRect,
+		box,
+		0.0f,
+		this->color
+	);
+	*/
 }
 
 
 void MainProgram::drawSceneCreationScreen()
 {
-	static const unsigned int background = Bengine::ResourceManager::getTexture("Textures/newscene.png").id;
+	static const unsigned int addSceneBackground = Bengine::ResourceManager::getTexture("Textures/newscene.png").id;
+	static const unsigned int addDialogueBackground = Bengine::ResourceManager::getTexture("Textures/newdialogue.png").id;
 	static const unsigned int cancelButton = Bengine::ResourceManager::getTexture("Textures/cancel-button.png").id;
 	static const unsigned int createButton = Bengine::ResourceManager::getTexture("Textures/create-button.png").id;
 
 	// Background (banner, bg this->color)
-	this->spriteBatch.draw(
-		this->mainBgDestRect,
-		this->mainUvRect,
-		background,
-		0.0f,
-		this->color
-	);
+	if (this->currentState == ProgramState::ADDSCENE) {
+		this->spriteBatch.draw(
+			this->mainBgDestRect,
+			this->mainUvRect,
+			addSceneBackground,
+			0.0f,
+			this->color
+		);
+	}
+	else if (this->currentState == ProgramState::ADD_DIALOGUE) {
+		this->spriteBatch.draw(
+			this->mainBgDestRect,
+			this->mainUvRect,
+			addDialogueBackground,
+			0.0f,
+			this->color
+		);
+	}
 
 	// Cancel button
 	this->spriteBatch.draw(
@@ -443,9 +612,9 @@ void MainProgram::drawMainScreenTexts()
 	this->fontBatch.begin();
 
 	// Get scenes to be shown
-	std::map<int, std::pair<std::string, Dialogue>> scenes = this->getShownScenes(this->sceneManager->getScenes());
+	std::map<int, std::pair<std::string, std::vector<Dialogue *>>> scenes = this->getShownScenes(this->sceneManager->getScenes());
 
-	int currentY = 191;
+	int currentY = 196;
 	for (unsigned i = 0; i < scenes.size(); i++) {
 		// Fill the buffer with the text
 		sprintf_s(buffer, "%s", scenes[i].first.c_str());
@@ -459,7 +628,7 @@ void MainProgram::drawMainScreenTexts()
 			Bengine::ColorRGBA8(0, 0, 0, 255)
 		);
 
-		currentY += 87;
+		currentY += 90;
 	}
 
 	this->fontBatch.end();
@@ -499,21 +668,23 @@ void MainProgram::drawSceneCreationScreenTexts()
 }
 
 
-std::map<int, std::pair<std::string, Dialogue>> MainProgram::getShownScenes(std::map<int, std::pair<std::string, Dialogue>> allScenes)
+std::map<int, std::pair<std::string, std::vector<Dialogue *>>> MainProgram::getShownScenes(std::map<int, std::pair<std::string, std::vector<Dialogue *>>> allScenes)
 {
-	std::map<int, std::pair<std::string, Dialogue>> shownScenes;
+	std::map<int, std::pair<std::string, std::vector<Dialogue *>>> shownScenes;
+	std::vector<int> indexes;
 
-	// If there's more scenes that can be shown, only pick 4
-	if (allScenes.size() > 4) {
+	// If there's more scenes that can be shown, only pick 5
+	if (allScenes.size() > 5) {
 		// Get 4 items from the map
-		auto end = allScenes.end();
 		int counter = 0;
 
-		for (auto it = allScenes.begin(); it != end; it++) {
+		for (auto it = allScenes.begin(); it != allScenes.end(); it++) {
 			if (counter == this->currentSceneListIdx) {
 				// Put the 4 scenes that will be shown into the 'shownScenes' vector
-				for (unsigned i = 0; i < 4; i++) {
+				for (unsigned i = 0; i < 5; i++) {
 					shownScenes.emplace(i, it->second);
+					indexes.push_back(it->first);
+
 					it++;
 				}
 
@@ -524,15 +695,32 @@ std::map<int, std::pair<std::string, Dialogue>> MainProgram::getShownScenes(std:
 		}
 	}
 
-	if (shownScenes.size() == 0) return allScenes;
-	else return shownScenes;
+	this->shownSceneIndexes.clear();
+
+	if (shownScenes.size() == 0) {
+		// Store shown scene indexes
+		for (auto it = allScenes.begin(); it != allScenes.end(); it++) {
+			this->shownSceneIndexes.push_back(it->first);
+		}
+
+		return allScenes;
+	}
+	else {
+		// Store shown scene indexes
+		this->shownSceneIndexes = indexes;
+
+		return shownScenes;
+	}
 }
 
 
-glm::vec4 MainProgram::getInputDimensions(glm::vec4 texture)
+glm::vec4 MainProgram::getInputDimensions(glm::vec4 texture, bool swapy)
 {
 	texture.x = texture.x + this->screenWidth / 2;
-	texture.y = abs((texture.y + this->screenHeight / 2) - this->screenHeight) - texture.a; // Flips the y value
+
+	if (swapy) {
+		texture.y = abs((texture.y + this->screenHeight / 2) - this->screenHeight) - texture.a; // Flips the y value
+	}
 
 	return texture;
 }

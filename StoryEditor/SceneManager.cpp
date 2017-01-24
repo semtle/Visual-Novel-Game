22e42ae0080216576b3ac06403c65316e6b13a78
Zentryn <veikkis98@gmail.com>
2017-01-24 09:13:27 +0200

    Addded basic saving of the story made in the editor

--- StoryEditor/SceneManager.cpp
+++ StoryEditor/SceneManager.cpp
@@ -44,8 +44,51 @@ void SceneManager::saveScenes(const std::string& filePath)
 {
 	std::ofstream file(filePath);
 
-	for (unsigned i = 0; i < this->scenes.size(); i++) {
-		file << this->scenes[i].first << "\n";
+	if (file.is_open()) {
+		for (unsigned i = 0; i < this->scenes.size(); i++) {
+			file << this->scenes[i].first << ":\n";
+
+			std::vector<Dialogue *> dialogues = this->scenes[i].second;
+			for (unsigned j = 0; j < dialogues.size(); j++) {
+				file << "    " << dialogues[j]->name << ":\n";
+
+				// A normal dialogue
+				if (!dialogues[j]->question) {
+					file << "        " << "talking: " << dialogues[j]->talking << "\n";
+					file << "        " << "left: " << dialogues[j]->left << "\n";
+					file << "        " << "right: " << dialogues[j]->right << "\n";
+					file << "        " << "message: " << dialogues[j]->message << "\n";
+
+					// Default next
+					if (j != dialogues.size() - 1)
+						file << "        " << "next: " << dialogues[j + 1]->name << "\n";
+					else if (i != this->scenes.size() - 1)
+						file << "        " << "next_scene: " << this->scenes[i + 1].first << "\n";
+				}
+				// A question
+				else {
+					file << "        " << "asking: " << dialogues[j]->charAskingQuestion << "\n";
+
+					// First option
+					file << "        " << "Option 1:\n";
+					file << "            " << "text: " << dialogues[j]->option1Text << "\n";
+					file << "            " << "influence: " << dialogues[j]->option1Influence << "\n";
+					file << "            " << "next: " << dialogues[j]->option1Next << "\n";
+
+					// Second option
+					file << "        " << "Option 2:\n";
+					file << "            " << "text: " << dialogues[j]->option2Text << "\n";
+					file << "            " << "influence: " << dialogues[j]->option2Influence << "\n";
+					file << "            " << "next: " << dialogues[j]->option2Next << "\n";
+
+					// Third option
+					file << "        " << "Option 3:\n";
+					file << "            " << "text: " << dialogues[j]->option3Text << "\n";
+					file << "            " << "influence: " << dialogues[j]->option3Influence << "\n";
+					file << "            " << "next: " << dialogues[j]->option3Next << "\n";
+				}
+			}
+		}
 	}
 
 	file.close();

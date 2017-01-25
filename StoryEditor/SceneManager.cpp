11110bc175fe8485f178026fa125ebf65f183ec1
Zentryn <veikkis98@gmail.com>
2017-01-25 09:33:31 +0200

    Improved file saving

--- StoryEditor/SceneManager.cpp
+++ StoryEditor/SceneManager.cpp
@@ -47,26 +47,67 @@ void SceneManager::saveScenes(const std::string& filePath)
 	if (file.is_open()) {
 		std::cout << "Saving...\n";
 
+		if (this->scenes.size() > 0) {
+			file << "Start:\n";
+			file << "    next_scene: " << this->scenes[0].first << "\n";
+		}
+
 		for (unsigned i = 0; i < this->scenes.size(); i++) {
 			file << this->scenes[i].first << ":\n";
 			file << "    " << "Background: " << this->sceneBackgrounds[i] << "\n";
 
 			std::vector<Dialogue *> dialogues = this->scenes[i].second;
+			
+			if (dialogues.size() > 0) {
+				file << "    " << "Start:\n";
+				file << "        " << "next: " << dialogues[0]->name << "\n";
+			}
+
 			for (unsigned j = 0; j < dialogues.size(); j++) {
 				file << "    " << dialogues[j]->name << ":\n";
 
 				// A normal dialogue
 				if (!dialogues[j]->question) {
-					file << "        " << "talking: " << dialogues[j]->talking << "\n";
-					file << "        " << "left: " << dialogues[j]->left << "\n";
-					file << "        " << "right: " << dialogues[j]->right << "\n";
-					file << "        " << "message: " << dialogues[j]->message << "\n";
+					if (dialogues[j]->talker)
+						file << "        " << "talking: " << dialogues[j]->talking << "\n";
+
+					if (dialogues[j]->left != "")
+						file << "        " << "left: " << dialogues[j]->left << "\n";
+
+					if (dialogues[j]->right != "")
+						file << "        " << "right: " << dialogues[j]->right << "\n";
+
+					if (dialogues[j]->showTextBox)
+						file << "        " << "message: " << dialogues[j]->message << "\n";
 
 					// Default next
-					if (j != dialogues.size() - 1)
-						file << "        " << "next: " << dialogues[j + 1]->name << "\n";
-					else if (i != this->scenes.size() - 1)
-						file << "        " << "next_scene: " << this->scenes[i + 1].first << "\n";
+					if (dialogues[j]->nextDialogue == "") {
+						if (j != dialogues.size() - 1)
+							file << "        " << "next: " << dialogues[j + 1]->name << "\n";
+						else if (i != this->scenes.size() - 1)
+							file << "        " << "next_scene: " << this->scenes[i + 1].first << "\n";
+					}
+					// Custom next
+					else {
+						std::string endPart = dialogues[j]->nextDialogue;
+
+						if (endPart != "Next Dialogue Not Set") {
+							endPart = endPart.substr(endPart.find(",") + 1, endPart.length());
+							std::string sceneName = endPart.substr(0, endPart.find(","));
+
+							// If the dialogue is in the current scene
+							if (sceneName == this->scenes[i].first) {
+								 std::string dialogueName = endPart.substr(endPart.find(",") + 1, endPart.length());
+								 file << "        " << "next: " << dialogueName << "\n";
+							}
+							// If the dialogue is in another scene
+							else {
+								file << "        " << "next_scene: " << sceneName << "\n";
+							}
+						}
+
+						
+					}
 				}
 				// A question
 				else {

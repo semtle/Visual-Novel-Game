dd055a40935bfba9c35aed6963484f09fb12eef9
Zentryn <veikkis98@gmail.com>
2017-01-25 09:28:53 +0200

    Changed init function to take file name as a parameter

--- Visual Novel Game/Scene.cpp
+++ Visual Novel Game/Scene.cpp
@@ -12,6 +12,6 @@ Scene::~Scene()
 }
 
 
-void Scene::init(Bengine::InputManager* manager, const std::map<std::string, Character *>& characters, const int& screenWidth, const int& screenHeight)
+void Scene::init(const std::string &fileName, Bengine::InputManager* manager, const std::map<std::string, Character *>& characters, const int& screenWidth, const int& screenHeight)
 {
 }

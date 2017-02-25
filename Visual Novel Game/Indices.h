b434645f119f496dda7f97379d12e2c03fa9223e
Zentryn <veikkis98@gmail.com>
2017-02-26 01:51:27 +0200

    Added new game screen

--- Visual Novel Game/Indices.h
+++ Visual Novel Game/Indices.h
@@ -2,4 +2,5 @@
 
 const int SCREEN_INDEX_LOADING_SCREEN = 0;
 const int SCREEN_INDEX_MAIN_MENU = 1;
-const int SCREEN_INDEX_PLAY = 2;
+const int SCREEN_INDEX_NEW_GAME = 2;
+const int SCREEN_INDEX_PLAY = 4;

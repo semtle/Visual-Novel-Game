d90e5e715ff70d9231d1f68a399af93a1067f524
Zentryn <veikkis98@gmail.com>
2017-02-26 21:33:35 +0200

    Added method that replaces double backslashes with single forward slashes so when loading textures, can just write the normal way

--- Visual Novel Game/LoadingFilesFetcher.h
+++ Visual Novel Game/LoadingFilesFetcher.h
@@ -7,4 +7,7 @@
 class LoadingFilesFetcher {
 public:
     static std::vector<std::string> getMenuLoadingScreenImagePaths();
+private:
+    // Replaces all double backslashes with a single forward slash
+    static void fixPathSlashes(std::vector<std::string>& paths);
 };

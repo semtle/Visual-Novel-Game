8f4a62514ca4dd856aff86c2836d205fa9fb917b
Zentryn <veikkis98@gmail.com>
2017-02-28 04:20:22 +0200

    Fixed gameplay screen's index

--- Visual Novel Game/Indices.h
+++ Visual Novel Game/Indices.h
@@ -3,4 +3,4 @@
 const int SCREEN_INDEX_LOADING_SCREEN = 0;
 const int SCREEN_INDEX_MAIN_MENU = 1;
 const int SCREEN_INDEX_NEW_GAME = 2;
-const int SCREEN_INDEX_PLAY = 4;
+const int SCREEN_INDEX_PLAY = 3;

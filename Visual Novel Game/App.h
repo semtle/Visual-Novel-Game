c7f7a074e7b963115838188be5812df7b576b8c7
Zentryn <veikkis98@gmail.com>
2017-03-01 19:04:12 +0200

    Added menu song

--- Visual Novel Game/App.h
+++ Visual Novel Game/App.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Bengine/IMainGame.h>
+#include <Bengine/AudioEngine.h>
 #include <memory>
 #include <vector>
 #include <string>
@@ -20,7 +21,12 @@ public:
     virtual void addScreens() override;
     virtual void onExit() override;
 
+    void startMenuSong();
+    void endMenuSong();
 private:
+    Bengine::AudioEngine m_audioEngine;
+    Bengine::Music m_menuSong;
+
     std::unique_ptr<LoadingScreen> m_menuLoadingScreen = nullptr;
     std::unique_ptr<MainMenuScreen> m_mainMenuScreen = nullptr;
     std::unique_ptr<NewGameScreen> m_newGameScreen = nullptr;

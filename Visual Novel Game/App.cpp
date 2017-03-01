bb6d6d511b7017a97b58a7f98793f596d13afae8
Zentryn <veikkis98@gmail.com>
2017-03-01 19:04:09 +0200

    Added menu song

--- Visual Novel Game/App.cpp
+++ Visual Novel Game/App.cpp
@@ -12,15 +12,16 @@ App::~App()
 
 void App::onInit()
 {
-
+    m_audioEngine.init();
+    m_menuSong = m_audioEngine.loadMusic("Audio/Music/MenuSong.mp3");
 }
 
 void App::addScreens()
 {
     m_menuLoadingScreen = std::make_unique<LoadingScreen>(&m_window, LoadingFilesFetcher::getMenuLoadingScreenImagePaths());
-    m_mainMenuScreen = std::make_unique<MainMenuScreen>(&m_window);
+    m_mainMenuScreen = std::make_unique<MainMenuScreen>(&m_window, this);
     m_mainGameScreen = std::make_unique<MainGameScreen>(&m_window);
-    m_newGameScreen = std::make_unique<NewGameScreen>(&m_window, m_mainGameScreen.get());
+    m_newGameScreen = std::make_unique<NewGameScreen>(&m_window, m_mainGameScreen.get(), this);
 
     m_screenList->addScreen(m_menuLoadingScreen.get());
     m_screenList->addScreen(m_mainMenuScreen.get());
@@ -33,4 +34,14 @@ void App::addScreens()
 void App::onExit()
 {
 
+}
+
+void App::startMenuSong()
+{
+    m_menuSong.play(-1);
+}
+
+void App::endMenuSong()
+{
+    m_menuSong.stop();
 }
